6dc9fe166a72b07c0527148dfa18df344635e422
dawid.ignacy1@gmail.com <dawid.ignacy1@gmail.com>
2021-09-28 09:07:15 +0200

    1

--- SunProject/WeatherForecast.cs
+++ SunProject/WeatherForecast.cs
@@ -7,7 +7,7 @@ namespace SunProject
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
-
+        //weather
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
