45b01397a585c7d65adcd0b7f4abbc978754d848
Adameeek <mazur.adam1507@gmail.com>
2021-10-03 17:47:24 +0200

    cleaning code

--- SunProject/Controllers/CalculatorEnergyController.cs
+++ SunProject/Controllers/CalculatorEnergyController.cs
@@ -3,7 +3,6 @@ using SunProject.Classes;
 using SunProject.Models;
 using System.Collections.Generic;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SunProject.Controllers
 {
@@ -11,40 +10,13 @@ namespace SunProject.Controllers
     [ApiController]
     public class CalculatorEnergyController : ControllerBase
     {
-        // GET: api/<CalcEngController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<CalcEngController>/5
-        /* [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-        */
-        // POST api/<CalcEngController>
         [HttpPost("Calculator")]
-        public ActionResult Calculation(CalculatorEnergy request)
+        public ActionResult Calculation(CalculatorEnergy Calculation)
         {
             return Ok(new CalculatorResult
             {
                 ActualEnergyGain = 0
             }) ;
-        }
-
-        // PUT api/<CalcEngController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<CalcEngController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
